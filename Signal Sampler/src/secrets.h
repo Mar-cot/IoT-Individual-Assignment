0f8c70adab6ce77fc34644a973c11a09f2a19726
Mar-cot <75980836+Mar-cot@users.noreply.github.com>
2026-04-21 11:56:52 +0200

    - Added MQTT communication of aggregated value (using ThingsBoard Cloud) (Set QoS level to 1 in Espressif_MQTT_Client.h to allow RTT measuring).

--- Signal Sampler/src/secrets.h
+++
@@ -1,4 +0,0 @@
-const char* WIFI_SSID          = "YOUR_WIFI_SSID";
-const char* WIFI_PASSWORD      = "YOUR_WIFI_PASSWORD";
-const char* THINGSBOARD_SERVER = "thingsboard.cloud";
-const char* DEVICE_TOKEN       = "YOUR_DEVICE_TOKEN";
